d6de9924d888f79ce605fb05458d01dd034ad8d7
ZenRay <jake2011ren@gmail.com>
2018-11-17 02:57:02 +0800

    feat($wranglingData): Parse the running time
    use the function parse_time_value to get the time intervel in each transaction
    
    fixes #1

--- tool/preprocess.py
+++ tool/preprocess.py
@@ -3,7 +3,7 @@
 
 import pandas as pd
 import numpy as np
-
+from addiction import view_progress
 
 def parse_offer_id(x, option=["offer_id", "offer id"]):
     """Parse the offer id value
@@ -30,3 +30,64 @@ def parse_offer_id(x, option=["offer_id", "offer id"]):
         result = np.nan
     
     return result
+
+def parse_time_value(data, target_column, option="offer_id"):
+    """Parse the time interval and transaction time
+
+    Get the the transaction time interval, since the user finish the last 
+    transaction time.
+
+    Parameters:
+    -----------
+    data: DataFrame
+        Original data
+    target_column: string
+        The label string. If it exists, update the column value; otherwise, 
+        create the target_column
+    option: string default offer_id
+        It is additional option to group. It is used, when offer_id is validate
+        value; otherwise, it is deprecated, when offer_id is missing value
+    
+    Results:
+    ----------
+    result: DataFrame or Series
+        Store the information from parse the time
+    columns: list labels
+        Store the new labels
+    """
+    if target_column not in data:
+        result = pd.DataFrame(index=data.index)
+        result[target_column] = np.nan
+    else:
+        result = data[[target_column]]
+
+    person_with_offer = data.groupby(["person", option])["time"]
+    person_without_offer = \
+        data.loc[data[option].isnull()].groupby(["person"])["time"]
+    
+    # parse the time information about the validate offer_id value
+    person_offer_items = person_with_offer.apply(
+        lambda x: np.diff(sorted(x))
+    ).reset_index()
+    person_offer_groups = person_with_offer.groups
+
+    result["time_interval"] = view_progress(
+        person_offer_items, person_offer_groups, person_offer_items.shape[0],
+        result["time_interval"]
+    )
+    
+    # parse the time information about the invalidate offer_id value
+    person_without_offer_items = person_without_offer.apply(
+        lambda x: np.diff(sorted(x))
+    ).reset_index()
+    person_without_offer_groups = person_without_offer.groups
+
+    result["time_interval"] = view_progress(
+        person_without_offer_items, person_without_offer_groups,
+        person_without_offer_items.shape[0], result["time_interval"]
+    )
+
+    # get the labels
+    columns = result.columns
+
+    return result, columns
